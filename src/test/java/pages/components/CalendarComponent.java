54b1a1ec606405c1be49470671373a9f6dc6efc2
Your Name <you@example.com>
2025-12-25 23:35:46 +0700

    added Tests

---
+++ src/test/java/pages/components/CalendarComponent.java
@@ -0,0 +1,13 @@
+package pages.components;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CalendarComponent {
+    public void setDate(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("March")).click();
+        $(".react-datepicker__year-select").$(byText("1991")).click();
+        $(".react-datepicker__month").$(byText("19")).click();
+    }
+}
